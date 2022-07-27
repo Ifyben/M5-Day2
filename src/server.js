3a0cabbb8a9156116b68c174559eee09b3459394
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-07-27 18:11:21 +0200

    commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express"
+import cors from "cors"
+import listEndpoints from "express-list-endpoints"
+
+const server = express();
+const PORT = 3001;
+server.use(cors());
+server.use(express.json());
+console.log(listEndpoints(server));
+server.listen(PORT, () => console.log("✅ server is running on port : ", PORT));
+server.on("error", (error) => 
+   console.log(`❌ server is not running due to : ${error}`)
+   );
