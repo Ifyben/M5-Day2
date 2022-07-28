9c85351d98511b78a8f4d487029605e2c7e9fdd9
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-07-28 11:33:24 +0200

    commit

--- src/authors/index.js
+++ src/authors/index.js
@@ -1,10 +1,20 @@
 import express from "express"
+import fs from "fs"
+import path,{dirname} from "path";
+import { fileURLToPath } from "url"
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const authorsFilePath = path.join(__dirname, "authors.json");
 const router = express.Router();
 
 // get all authors
 router.get("/", async (req, res, next) => {
     try {  
+      const fileAsBuffer = fs.readFileSync(authorsFilePath);
+      const fileAsString = fileAsBuffer.toString();
+      const fileAsJSON = JSON.parse(fileAsString);
+      res.send(fileAsJSON);
     } catch (error) {
       res.send(500).send({ message: error.message });  
     }
