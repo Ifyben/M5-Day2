7523ac923ea8ada7bac483d0ade13ead1be18899
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-07-29 20:06:57 +0200

    commit

--- src/errorHandlers.js
+++ src/errorHandlers.js
@@ -1,9 +1,25 @@
+const notFound = (err, req, res, next) => {
+    if(err && err.status === 404) {
+        res.status(404).send({ message: err.message || "Not Found" })
+    }
+    next();
+}
+
+
+const forbidden = (err, req, res, next) => {
+    if(err && err.status === 403) {
+        res.status(403).send({ message: err.message || "Forbidden!" });
+    }
+    next();
+}
+
+
 const catchAllErrorHandler = (err, req, res, next) => {
     if(err) {
         if (!req.headersSent) {
             res
               .status(err.status || 500)
-              .send({ message: err.message || "Something went wrong!"});
+              .send({ message: err.message || "Something went wrong!" });
         }
     } 
     next();
