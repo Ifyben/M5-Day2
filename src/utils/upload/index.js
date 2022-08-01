ca263faa31457bee3ecdf5184e8ea00efbaa9b57
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-08-01 21:21:02 +0200

    commit

--- src/utils/upload/index.js
+++ src/utils/upload/index.js
@@ -2,8 +2,20 @@ import path,{dirname} from "path";
 
 import { fileURLToPath } from "url";
 
+import multer from "multer"; 
+
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = dirname(__filename);
 
 const publicDirectory = path.join(__dirname, "../../public");
+
+const parseFile = multer(); 
+
+const uploadFile = (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
